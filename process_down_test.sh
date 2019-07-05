9a3d1c4805e9ffbbb63f63c7e44b7a5a5e02427e
yonedakenji <52304035+yonedakenji@users.noreply.github.com>
2019-07-05 14:05:09 +0900

    Update process_down_test.sh

--- process_down_test.sh
+++ process_down_test.sh
@@ -3,25 +3,14 @@ RESTART_CONTAINER=process_down_test_02
 COUNT=100
 
 # check ps output.
-# /usr/libexec/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib64/mysql/plugin --user=mysql --log-error=/var/log/mariadb/mariadb.log --pid-file=/var/run/mariadb/mariadb.pid --socket=/var/lib/mysql/mysql.sock
-# MYSQL_PS="/usr/libexec/mysqld "
-# /system/MW/java/bin/java -Djava.util.logging.config.file=/system/MW/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djdk.tls.ephemeralDHKeySize=2048 -Djava.protocol.handler.pkgs=org.apache.catalina.webresources -Dignore.endorsed.dirs= -classpath /system/MW/tomcat/bin/bootstrap.jar:/system/MW/tomcat/bin/tomcat-juli.jar -Dcatalina.base=/system/MW/tomcat -Dcatalina.home=/system/MW/tomcat -Djava.io.tmpdir=/system/MW/tomcat/temp org.apache.catalina.startup.Bootstrap start
-# TOMCAT_PS="/system/MW/java/bin/java "
-# /system/MW/apache/bin/httpd -DNO_DETACH
-HTTPD_PS="httpd -DNO_DETACH"
+PS_1="httpd -DNO_DETACH"
 
 function docker_top_check {
     docker top $1 2>&1 > $1.top
-#    mysql_exist=`grep --count "${MYSQL_PS}" $1.top`
-#    tomcat_exist=`grep --count "${TOMCAT_PS}" $1.top`
-    httpd_exist=`grep --count "${HTTPD_PS}" $1.top`
-#    echo "$1 mysql_exist:$mysql_exist"
-#    echo "$1 tomcat_exist:$tomcat_exist"
-    echo "$1 httpd_exist:$httpd_exist"
-#    echo "$1 mysql_exist * tomcat_exist * httpd_exist:$((mysql_exist * tomcat_exist * httpd_exist))"
+    PS_1_exist=`grep --count "${PS_1}" $1.top`
+    echo "$1 ${PS_1}:${PS_1_exist}"
 
-#    if [ $((mysql_exist * tomcat_exist * httpd_exist)) -eq 0 ]; then
-    if [ $httpd_exist -eq 0 ]; then
+    if [ ${PS_1_exist} -eq 0 ]; then
         echo "exit"
         exit -1
     fi
